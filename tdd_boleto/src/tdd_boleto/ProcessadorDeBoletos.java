377e30b093c780b39d0556fb2e79069ae2b1cd94
Aline Lima <44929019+Alines009@users.noreply.github.com>
2022-04-24 14:42:52 -0300

    Alteração da função para receber uma lista de Boleto como parâmetro.

--- tdd_boleto/src/tdd_boleto/ProcessadorDeBoletos.java
+++ tdd_boleto/src/tdd_boleto/ProcessadorDeBoletos.java
@@ -1,11 +1,17 @@
 package tdd_boleto;
 
+import java.util.ArrayList;
+
 public class ProcessadorDeBoletos {
 	
-		public boolean realizaPagamento(Boleto boleto, Fatura fatura) {
-			double valorBoleto = boleto.getValorPago();
+		public boolean realizaPagamento(ArrayList<Boleto> boletosFatura, Fatura fatura) {
+			double valorPago = 0.0;
+			for(int i = 0; i < boletosFatura.size(); i++) {
+				valorPago += boletosFatura.get(i).getValorPago();
+			}
+			
 			double valorFatura = fatura.getValorFatura();
-			if(valorBoleto == valorFatura) {
+			if(valorPago == valorFatura) {
 				return true;
 			}
 			return false;
